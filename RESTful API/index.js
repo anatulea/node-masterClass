1bb3d2423484740f86200d9d0488f4006391911b
Ana Tulea <anatulea14@gmail.com>
2021-02-18 11:25:11 -0800

    Parsing Request Paths

--- RESTful API/index.js
+++ RESTful API/index.js
@@ -6,12 +6,23 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // Configure the server to respond to all requests with a string
 const server = http.createServer(function(req, res){
-    res.end('Hello world!\n')
-})
+    // Get the url and parse it
+    let parsedUrl = url.parse(req.url, true); // url.parse() Stability: 0 - Deprecated: Use the WHATWG URL API instead.
+
+    // Get the path 
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    // Send the response
+    res.end('Hello world!\n');
+    
+    // Log the request path
+    console.log('Request received on path: '+trimmedPath);
+});
 // Start the server
 server.listen(3000, function(){
-    console.log('The server is listening on port 3000 now')
-})
+    console.log('The server is listening on port 3000 now');
+});
