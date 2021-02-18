c9a038826190cc87ad7578a45c536f9871569758
Ana Tulea <anatulea14@gmail.com>
2021-02-18 11:08:23 -0800

    Starting a Server

--- RESTful API/index.js
+++ RESTful API/index.js
@@ -1,1 +1,17 @@
-console.log('Hello world')
+/*
+*
+*Primary file for the API
+*
+*/
+
+// Dependencies
+const http = require('http');
+
+// Configure the server to respond to all requests with a string
+const server = http.createServer(function(req, res){
+    res.end('Hello world!\n')
+})
+// Start the server
+server.listen(3000, function(){
+    console.log('The server is listening on port 3000 now')
+})
