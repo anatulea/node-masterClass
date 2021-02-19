bb26bfedcfb2f5d35376fbd41bea13cedf402243
Ana Tulea <anatulea14@gmail.com>
2021-02-18 18:14:24 -0800

    Parsing Payloads

--- RESTful API/index.js
+++ RESTful API/index.js
@@ -6,34 +6,46 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
- // Configure the server to respond to all requests with a string
-const server = http.createServer(function(req,res){
+// Configure the server to respond to all requests with a string
+const server = http.createServer(function (req, res) {
 
-  // Parse the url
-  const parsedUrl = url.parse(req.url, true);
+    // Parse the url
+    const parsedUrl = url.parse(req.url, true);
 
-  // Get the path
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-  // Get the query string as an object
-  const queryStringObject = parsedUrl.query;
+    // Get the query string as an object
+    const queryStringObject = parsedUrl.query;
 
-  // Get the HTTP method
-  const method = req.method.toLowerCase();
+    // Get the HTTP method
+    const method = req.method.toLowerCase();
 
-  //Get the headers as an object
-  const headers = req.headers;
+    //Get the headers as an object
+    const headers = req.headers;
 
-  // Send the response
-  res.end('Hello World!\n');
+    // Get the payload,if any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
 
-  // Log the request/response
-  console.log('Request received with these headers: ',headers);
+    req.on('data', function (data) {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', function () {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end('Hello World!\n');
+
+        // Log the request/response
+        console.log('Request received with this payload: ', buffer);
+    });
 });
-
 // Start the server
-server.listen(3000,function(){
-  console.log('The server is up and running now');
+server.listen(3000, function () {
+    console.log('The server is up and running now');
 });
