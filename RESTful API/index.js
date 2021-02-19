a1820eeaa6d661ebae1a8901fe9ff3a0b447be07
Ana Tulea <anatulea14@gmail.com>
2021-02-18 18:01:55 -0800

    Parsing Query Strings

--- RESTful API/index.js
+++ RESTful API/index.js
@@ -1,33 +1,35 @@
 /*
-*
-*Primary file for the API
-*
-*/
+ * Primary file for API
+ *
+ */
 
 // Dependencies
-const http = require('http');
-const url = require('url');
+var http = require('http');
+var url = require('url');
 
-// Configure the server to respond to all requests with a string
-const server = http.createServer(function(req, res){
-    // Get the url and parse it
-    let parsedUrl = url.parse(req.url, true); // url.parse() Stability: 0 - Deprecated: Use the WHATWG URL API instead.
+ // Configure the server to respond to all requests with a string
+var server = http.createServer(function(req,res){
 
-    // Get the path 
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  // Parse the url
+  var parsedUrl = url.parse(req.url, true);
 
-    // Get the http method
-    const method =  req.method.toLowerCase()
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+  // Get the query string as an object
+  var queryStringObject = parsedUrl.query;
 
-    // Send the response
-    res.end('Hello world!\n');
-    
-    // Log the request path
-    console.log('Request received on path: '+trimmedPath+ ' with the method: '+method);
+  // Get the HTTP method
+  var method = req.method.toLowerCase();
+
+  // Send the response
+  res.end('Hello World!\n');
+  // Log the request/response
+  console.log('Request received on path: ', trimmedPath, ' with method: ', method, ' and this query string: ',queryStringObject);
 });
+
 // Start the server
-server.listen(3000, function(){
-    console.log('The server is listening on port 3000 now');
+server.listen(3000,function(){
+  console.log('The server is up and running on port 3000 now');
 });
