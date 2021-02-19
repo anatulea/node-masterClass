f4d668a24426964afc76b0bfc034a5fae6c2cffd
Ana Tulea <anatulea14@gmail.com>
2021-02-18 18:54:17 -0800

    Service 1 - Ping

--- RESTful API/index.js
+++ RESTful API/index.js
@@ -103,10 +103,8 @@ const unifiedServer = function (req, res) {
 const handlers = {};
 
 // Sample handler
-handlers.sample = function (data, callback) {
-    callback(406, {
-        'name': 'sample handler'
-    });
+handlers.ping = function (data, callback) {
+    callback(200)
 };
 
 // Not found handler
@@ -116,5 +114,5 @@ handlers.notFound = function (data, callback) {
 
 // Define the request router
 const router = {
-    'sample': handlers.sample
+    'ping': handlers.ping
 };
